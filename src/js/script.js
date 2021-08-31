2b53a04eb86cf8ba6642cf6edf8287231c8768e1
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-08-31 20:12:54 +0300

    add page navigation

--- src/js/script.js
+++ src/js/script.js
@@ -41,3 +41,14 @@ document.addEventListener('keydown', function (e) {
 btnScrollTo.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+// Page navigation
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
